7a0a16ffa69fa2682c754c7721e3036792ccdc30
GHPrince07 <kiparang87@hanmail.net>
2023-10-22 23:53:00 +0900

    231022_2352
    백준_진법 변환 1

--- gk_test.py
+++ gk_test.py
@@ -1,3 +1,17 @@
 n, b = input().split()
 b = int(b)
+n = list(n)
+alphabet_list = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+num_list = range(10, 36)
 
+alphabet_dic = {}
+for alphabet, num in zip(alphabet_list, num_list):
+    alphabet_dic[alphabet] = num
+
+sum = 0
+for id, n_alphabet in enumerate(n):
+    if n_alphabet in alphabet_list:
+        sum += alphabet_dic[n_alphabet]*b**(len(n)-1-id)
+    else:
+        sum += int(n_alphabet)*b**(len(n)-1-id)
+print(sum)
